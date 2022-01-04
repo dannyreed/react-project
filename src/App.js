f359f7202aa2011268a5b3b8da0921c73ae176a5
Danny R <dreed3d3@gmail.com>
2022-01-03 17:29:21 -0800

    added a bunch of pages to the blog

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import Header from './header/Header'
 import TopBar from './topbar/TopBar'
 import Homepage from './pages/home/Home'
+import Single from './pages/single/Single'
+import Write from './pages/write/Write'
+import Settings from './pages/settings/Settings'
+import Login from './pages/login/Login'
 import './App.css';
 
 function App() {
   return (
     <>
       <TopBar />
-      <Homepage />
+      <Login />
     </>
   );
 }
